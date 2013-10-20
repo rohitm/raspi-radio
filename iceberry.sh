7c933bca571d54aed0aac1f9e66c24a940018129
rohitm <rohit.manohar@gmail.com>
2013-10-20 16:13:13 +0530

    changes to the start script

--- iceberry.sh
+++ iceberry.sh
@@ -5,13 +5,17 @@
 echo "Starting snd_bcm2835..."
 sudo modprobe snd_bcm2835
 
+sleep 5
+
 # Set the volume a little low
 echo "Lowering the volume..."
 amixer -c 0 set PCM 90%
 
+sleep 5
+
 # Start the node process
 echo "Starting Iceberry..."
-forever start /home/pi/apps/raspi-radio/app.js -o /home/pi/apps/raspi-radio/output.out
+forever start /home/pi/apps/raspi-radio/app.js > /home/pi/apps/raspi-radio/output.out
 
 exit 0
 
