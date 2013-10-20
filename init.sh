d3230dea859372d2b8b0059599b01ac305b33506
rohitm <rohit.manohar@gmail.com>
2013-10-20 13:36:06 +0530

    init script added

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ echo "Starting snd_bcm2835..."
 
 # Set the volume a little low
 echo "Lowering the volume..."
-`amixer -c 0 set PCM 90%`
+amixer -c 0 set PCM 90%
 
 # Start the node process
 echo "Starting iceberry..."
