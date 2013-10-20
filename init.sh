334eae8ead037fe69e718662a93167df75078020
rohitm <rohit.manohar@gmail.com>
2013-10-20 11:40:13 +0530

    init script added

--- init.sh
+++ init.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+
 # Start snd_bcm2835 module
-echo "Starting snd_bcm2835...\n"
-$(sudo modprobe snd_bcm2835)
+echo "Starting snd_bcm2835..."
+`sudo modprobe snd_bcm2835`
 
 # Set the volume a little low
-echo "Lowering the volume...\n"
-$(amixer -c 0 set PCM 90%)
+echo "Lowering the volume..."
+`amixer -c 0 set PCM 90%`
 
 # Start the node process
-echo "Starting iceberry...\n"
+echo "Starting iceberry..."
 $(nohup node /home/pi/apps/raspi-radio/app.js)
 
+exit
 
